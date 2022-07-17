8d191bffc6de0d679bf92d0025ff350029d42847
jacoposergio <djago32@gmail.com>
2022-07-17 15:06:14 +0200

    css

--- src/components/OurServices.vue
+++ src/components/OurServices.vue
@@ -1,6 +1,52 @@
 <template>
-   <section id="our-services">
-     our-services
+   <section id="our-services" class="row mb-0">
+      <div class="ms_container">
+        <div class="main_title">
+           <h3>See our Top Notch Services</h3>
+           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. molestias iure repellat neque.molestias iure repellat neque.</p>
+        </div>
+        <div class="cards_container">
+          <div class="row g-4 row-cols-4">
+
+                <div class="col">
+                  <div class="ms_card d-flex flex-column align-items-center">
+                     <div class="symbol"><i class="fa-brands fa-google"></i></div>
+                     <h4>Google SEO</h4>
+                     <p>Nesciunt magnam exercitationem obcaecati officia quos officiis vel excepturi, facilis nihil numquam et!</p>
+                     <a href="#">See more</a>
+                  </div>
+               </div>
+
+             <div class="col">
+                  <div class="ms_card d-flex flex-column align-items-center">
+                     <div class="symbol"><i class="fa-solid fa-gear"></i></div>
+                     <h4>Brand Strategy</h4>
+                     <p>Nesciunt magnam exercitationem obcaecati officia quos officiis vel excepturi, facilis nihil numquam et!</p>
+                     <a href="#">See more</a>
+                  </div>
+               </div>
+
+               <div class="col">
+                  <div class="ms_card d-flex flex-column align-items-center">
+                     <div class="symbol"><i class="fa-solid fa-building"></i></div>
+                     <h4>Local SEO</h4>
+                     <p>Nesciunt magnam exercitationem obcaecati officia quos officiis vel excepturi, facilis nihil numquam et!</p>
+                     <a href="#">See more</a>
+                  </div>
+               </div>
+
+                <div class="col">
+                  <div class="ms_card d-flex flex-column align-items-center">
+                     <div class="symbol"><i class="fa-solid fa-chart-column"></i></div>
+                     <h4>Seo Analysis</h4>
+                     <p>Nesciunt magnam exercitationem obcaecati officia quos officiis vel excepturi, facilis nihil numquam et!</p>
+                     <a href="#">See more</a>
+                  </div>
+               </div>
+
+          </div>
+        </div>  
+     </div>
    </section>
 </template>
 
@@ -13,5 +59,45 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+#our-services{
+      background-image: url(../assets/img/pattern_background.png);
+      background-color: blue;
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position-x: 50%;
+      background-position-y: 50%;
+      min-width: 800px;
+      color: white;
+}
+
+.ms_container{
+  padding-bottom: 150px;
+  
+  .main_title{
+   margin-bottom: 80px;
+  }
+
+.ms_card{
+    background-color: grey;
+
+      .symbol{
+         padding-top: 2rem;
+      }
+
+      h4{
+         margin-top: 1.5rem;
+         margin-bottom: 1rem;
+      }
+
+      p{
+         margin-bottom: 1rem;
+      }
+
+      a{
+        padding-bottom: 2rem;
+      }
+   }
+}
 
 </style>
